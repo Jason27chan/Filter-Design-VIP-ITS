0cc92ebbb86f7ac73d4c75a3372ea8c818b90cb8
Jason Chan <jason27chan@gmail.com>
2017-11-28 16:35:37 -0500

    change dummy text to actual questions

--- FilterDesign/FilterDesign.js
+++ FilterDesign/FilterDesign.js
@@ -344,8 +344,8 @@ $(document).ready(function() {
 
 });
 
-function addToDb() { 
-    var arr = printStuff();
+function recordGUIState() { 
+    var arr = printInfo();
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -373,3 +373,17 @@ function printInfo() {
     console.log("window: " +  wind);
     return new Array(tab, dropdown, fsamp, fcutoff, order, wind);
 }
+
+function recordAnswers() {
+    printAnswers();
+}
+
+function printAnswers() {
+    var arr = new Array();
+    for (var i = 1; i <= 5; i++) {
+        arr[0] = $("#q" + i).val();
+    } 
+    console.log(arr);
+}
+
+
